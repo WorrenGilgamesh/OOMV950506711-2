65a7fb2bb682765006bb720703416c3a4dd230ab
sdist <sdist@itam.mx>
2018-04-20 08:52:49 -0500

    esperemos que funcione maven

---
+++ prueba/src/test/java/TestCadena.java
@@ -0,0 +1,14 @@
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestCadena{
+    solu bla = new solu();
+    @Test
+    public void prueba1(){
+        System.out.println("Estoy en prueba ");
+        }
+    }
